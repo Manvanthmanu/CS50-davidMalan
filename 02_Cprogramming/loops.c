1e4c40074a8f63abf5abe64e8d04196e70be0588
Manvanth mn <21sjccc039_manvanth@sjcc.edu.in>
2024-09-03 01:24:11 +0530

    functions

--- 02_Cprogramming/loops.c
+++ 02_Cprogramming/loops.c
@@ -2,6 +2,24 @@
 
 int main(){
 
-    
+    int manu=8;
+    while(manu>0){
+        printf("%d\n",manu);
+        manu--;
+    }
+    char str1[]="hello";
+
+    for(int i = 0;i<3;i++){
+        printf("%s\n",str1);
+    }
+
+    int hey= 0;
+    while(1){
+        printf("hello world %d\n" , hey);
+        if(hey==10){
+            break;
+        }
+        hey++;
+    }
     return 0;
 }
