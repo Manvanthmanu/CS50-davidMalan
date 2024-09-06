a5a833ceac5fdf881149f60088e898f72f107a9c
Manvanth mn <21sjccc039_manvanth@sjcc.edu.in>
2024-09-06 10:45:21 +0530

    completed c week 1

--- 02_Cprogramming/Problemset1/credit/temp.c
+++ 02_Cprogramming/Problemset1/credit/temp.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <cs50.h>
+
 
 int count(long long int number){
     int count = 0;
@@ -31,21 +33,55 @@ long long int reverse(long long int cardnum , int length ){
     return temp;
 }
 
+int print(long long int card){
+    long long int n = card;
+    long long int tn = n;
+
+    
+
+    int count = 0;
+    // printf("%lld\n" , n);
+
+    while(tn>0){
+        count++;
+        tn=tn/10;
+    }
+    if(count==15 && ((n >= 340000000000000  && n <= 349999999999999) || 
+                    (n >= 370000000000000  && n <=379999999999999 ))) {
+        printf("AMEX\n");
+    }else if(count==16 ){
+        if(n>5100000000000000 && n<5599999999999999){
+            printf("MASTERCARD\n");
+        }else if(n>=4000000000000000 && n<5000000000000000){
+            printf("VISA\n");
+        }else{
+            printf("INVALID\n");
+        }
+    }else if(count==13 && (n>=4000000000000 && n<5000000000000)){
+        printf("VISA\n");
+    }else{
+        printf("INVALID\n");
+    }
+
+    // printf("%d", count);
+}
 
 
-int main(){
+
+int main( void ){
 
 
 
-    long long int num = 4003600000000014;
+    long long int num = get_long_long("Number: ");
 
     int length = count(num);
     long long int reverseNum = reverse(num , length);
 
     int sum = 0 ;
-    int check = 0;
+    long long int check = 0;
+    int newsumLeftout = 0;
 
-    printf("%lld\n" , reverseNum);
+    // printf("%lld\n" , reverseNum);
 
     for(int i=1; i<=length ;i++){
         long long int tenspower = power(10 , ( length - i)) ;
@@ -55,13 +91,36 @@ int main(){
         if(i%2==0){
             sum = digit*2;
             check = check +  (sum*power(10 , (i-1)));
+        }else{
+            newsumLeftout = newsumLeftout +digit;
         }
         reverseNum = reverseNum - (tenspower*digit); 
 
         // printf("%d\n" , digit);
 
     }
+
+    int newsumMultiplied = 0;
+    int newlength = count(check);
+
+    for(int i = 1;i<=newlength ;i++){
+        long long int tenspower = power(10 , ( newlength - i)) ;
+
+        int digit = check/tenspower;
+
+        newsumMultiplied = newsumMultiplied + digit;
+
+        check = check - (tenspower*digit);
+    }
     
-    printf("\n%d", sum);
-    return 0;
+    // printf("\n%d", newsumMultiplied);
+    // printf("\n%d", newsumLeftout);
+
+    int finalvaluation = newsumLeftout +newsumMultiplied;
+
+    if(finalvaluation%10==0){
+        print(num);
+    }else{
+        printf("INVALID\n");
+    }
 }
